f3e40f5969fa0ce98cb0094b5b799f296a4cc759
arnavvsharmaaa <arnavsharma8079@gmail.com>
2026-01-07 23:17:27 +0530

    Add SortColors class to sort colors in an array
    Implement Dutch National Flag Algorithm to sort an array of colors represented by integers (0, 1, 2).

---
+++ Arrays/SortColors.java
@@ -0,0 +1,46 @@
+// Problem: Sort Colors
+// Approach: Dutch National Flag Algorithm
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+public class SortColors {
+
+    public static void sortColors(int[] nums) {
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
